3450a23c02ac46c5242c122f51c868e8f7db9a2f
Ron Raithatha <ronaksraithatha@gmail.com>
2016-11-25 19:34:06 -0500

    One working solution

--- Arrays_and_Strings/Is_Unique/is_unique_1.py
+++ Arrays_and_Strings/Is_Unique/is_unique_1.py
@@ -1,2 +1,8 @@
-def is_unique_1():
-    return
+def is_unique_1(string):
+    seen_so_far = {}
+    for s in string:
+        if (s not in seen_so_far):
+            seen_so_far[s] = s
+        else:
+            return False
+    return True
---
+++ Arrays_and_Strings/Is_Unique/main.py
@@ -0,0 +1,8 @@
+from is_unique_1 import is_unique_1
+
+def main():
+    print "input: 'TestString', output: {output}".format(output=is_unique_1("TestString"))
+    print "input: 'abcdefgb', output: {output}".format(output=is_unique_1("abcdefgh"))
+
+if __name__ == '__main__':
+    main()
