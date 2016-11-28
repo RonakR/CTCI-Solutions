79a15d5484d8014ae0fd555b172da0b3541c2844
Ron Raithatha <ronaksraithatha@gmail.com>
2016-11-27 19:56:30 -0500

    fixed typo in markdown

--- Arrays_and_Strings/1_Is_Unique/main.py
+++ Arrays_and_Strings/1_Is_Unique/main.py
@@ -7,19 +7,19 @@ def main():
     print "input: '', output: {output}".format(output=is_unique_1(""))
     print "input: 1234, output: {output}".format(output=is_unique_1(1234))
     print "input: 'TestString', output: {output}".format(output=is_unique_1("TestString"))
-    print "input: 'abcdefgb', output: {output}\n".format(output=is_unique_1("abcdefgh"))
+    print "input: 'abcdefgh', output: {output}\n".format(output=is_unique_1("abcdefgh"))
 
     print "Solution 2: Sort string, compare adjacent characters"
     print "input: '', output: {output}".format(output=is_unique_2(""))
     print "input: 1234, output: {output}".format(output=is_unique_1(1234))
     print "input: 'TestString', output: {output}".format(output=is_unique_2("TestString"))
-    print "input: 'abcdefgb', output: {output}\n".format(output=is_unique_2("abcdefgh"))
+    print "input: 'abcdefgh', output: {output}\n".format(output=is_unique_2("abcdefgh"))
 
     print "Solution 3: (From book) 128 length array of False, mark true at ASCII value of current char"
     print "input: '', output: {output}".format(output=is_unique_book(""))
     print "input: 1234, output: {output}".format(output=is_unique_book(1234))
     print "input: 'TestString', output: {output}".format(output=is_unique_book("TestString"))
-    print "input: 'abcdefgb', output: {output}".format(output=is_unique_book("abcdefgh"))
+    print "input: 'abcdefgh', output: {output}".format(output=is_unique_book("abcdefgh"))
 
 if __name__ == '__main__':
     main()
