798a1b372a7d5eb5d32588dee36a61eea2d160ef
Ron Raithatha <ronaksraithatha@gmail.com>
2016-12-02 22:20:15 -0500

    small refactor

--- 2_Linked_Lists/1_Remove_Dups/remove_dups_main.py
+++ 2_Linked_Lists/1_Remove_Dups/remove_dups_main.py
@@ -3,16 +3,16 @@ from remove_dups_1 import remove_dups_1
 from remove_dups_2 import remove_dups_2
 
 def main():
-    items_1 = [1,2,4,5,3,4,1,1,5,5,6]
-    items_2 = ["alligator", "aardvark", "zebra", "zebra", "aardvark"]
+    items_1 = createLinkedList([1,2,4,5,3,4,1,1,5,5,6])
+    items_2 = createLinkedList(["alligator", "aardvark", "zebra", "zebra", "aardvark"])
 
     print "Solution 1: Run through list adding and comparing to a dictionary"
-    print "input: [1,2,4,5,3,4,1,5,5,6], output: {output}".format(output=remove_dups_1(createLinkedList(items_1)))
-    print "input: ['alligator', 'aardvark', 'zebra', 'zebra', 'aardvark'], output: {output}\n".format(output=remove_dups_1(createLinkedList(items_2)))
+    print "input: [1,2,4,5,3,4,1,5,5,6], output: {output}".format(output=remove_dups_1(items_1))
+    print "input: ['alligator', 'aardvark', 'zebra', 'zebra', 'aardvark'], output: {output}\n".format(output=remove_dups_1(items_2))
 
     print "Solution 2: For each character, use a runner to run through the rest of the list"
-    print "input: [1,2,4,5,3,4,1,5,5,6], output: {output}".format(output=remove_dups_2(createLinkedList(items_1)))
-    print "input: ['alligator', 'aardvark', 'zebra', 'zebra', 'aardvark'], output: {output}\n".format(output=remove_dups_2(createLinkedList(items_2)))
+    print "input: [1,2,4,5,3,4,1,5,5,6], output: {output}".format(output=remove_dups_2(items_1))
+    print "input: ['alligator', 'aardvark', 'zebra', 'zebra', 'aardvark'], output: {output}\n".format(output=remove_dups_2(items_2))
 
 
 def createLinkedList(items):
