1a4de3c390baf6c97b7c41c0844abacf5cc6dda7
Ron Raithatha <ronaksraithatha@gmail.com>
2016-12-08 21:09:12 -0500

    created seperated linkedlist and node classes

--- utilities/linked_list.py
+++ utilities/linked_list.py
@@ -1,11 +1,12 @@
-class Node:
+from utilities.node import Node
+
+class LinkedList:
     def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
+        self.head = Node(data, next)
 
     def __len__(self):
         length = 0
-        current_node = self
+        current_node = self.head
 
         while current_node is not None:
             length+=1
@@ -15,7 +16,7 @@ class Node:
 
     def __str__(self):
         max = 30
-        current_node = self
+        current_node = self.head
         retString = ""
         while current_node is not None and max:
             retString += str(current_node.data) + " "
@@ -26,7 +27,7 @@ class Node:
 
     def append(self, data):
         next_node = Node(data)
-        current_node = self
+        current_node = self.head
 
         while current_node.next is not None:
             current_node = current_node.next
@@ -34,7 +35,7 @@ class Node:
         current_node.next = next_node
 
     def append_full_node(self, node):
-        current_node = self
+        current_node = self.head
 
         while (current_node.next is not None):
             current_node = current_node.next
@@ -44,7 +45,7 @@ class Node:
 def create_linked_list(items):
     if not items: return "Empty list."
 
-    head = Node(items[0])
+    head = LinkedList(items[0])
     for item in items[1:]:
         head.append(item)
 
